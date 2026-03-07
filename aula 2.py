af51baca7483bb2bf80f17a58c2b7fcdbfd10526
VeronicaLira <liraveronica253@gmail.com>
2026-03-07 15:59:40 -0300

    Aula 2

---
+++ aula 2.py
@@ -0,0 +1,60 @@
+
+
+# nome=input('Digite seu nome:')
+
+# idade=input('Digite sua idade:')
+
+# print('Olá', nome, ', sua idade é:', idade)
+
+#cadastro de usuários
+# nome = input('Digite seu nome:')
+# idade= input('Digite sua idade:')
+# cpf=input('Digite seu CPF:')
+# endereco=input('Digite seu endereço:')
+
+# print(f'Cadastro realizado com sucesso.\n Nome:{nome}\n Idade:{idade}\n CPF:{cpf}\n Endereço:{endereco}')
+
+# x= int(input('Digite um número:'))
+
+# y= int(input('Digite um número:'))
+
+# print ('Números', x, 'e', y)
+
+# soma= x+y
+
+# print ('Soma:', soma)
+
+
+# subtracao= x-y
+
+# print('Subtração:', subtracao)
+
+# multiplicacao= x*y
+
+# print('Multiplicação:',multiplicacao)
+
+# divisao= x/y
+
+# print('Divisão:', divisao)
+
+# resto= x%y
+
+# print('Resto da divisão:', resto)
+
+# idade= 20
+
+# if idade >= 18:
+#     print('Pode dirigir')
+# else:
+#     print('Não pode dirigir')
+
+
+
+#Atividade
+
+x= int(input('Digite um número:'))
+
+if x > 0:
+    print('É positivo')
+else:
+    print('É negativo')
